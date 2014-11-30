33be9a7e545f16c26e1ff6083ebcf2f18651a279
Lars Walen <lars.walen@marshill.com>
2014-11-30 15:03:05 -0800

    Generate static can handle partials

--- generate_static.rb
+++ generate_static.rb
@@ -4,26 +4,40 @@
 
 require 'slim'
 
+def replace_slim_call(string)
+  string.gsub(/^( *={1,2} )slim :(.*)/, '\1 (f = File.read("views/\2.slim"); Slim::Template.new { f }.render)')
+end
+
+def prepare_layout
+  puts " :: views/layout.slim"
+
+  l = File.read("views/layout.slim")
+  l = replace_slim_call(l)
+  Slim::Template.new { l }
+end
+
 Slim::Engine.default_options[:pretty] = true
 
 puts "Creating static site..."
 
-l = File.read("views/layout.slim")
-layout = Slim::Template.new { l }
+layout = prepare_layout
 
 files = Dir['views/*']
 files.reject! { |f| f.gsub('views/', '').match(/^(layout|_)/) }
 
-puts files.inspect
-
 files.each do |file|
+
+  puts " :: #{file}"
+
   dir = File.basename(file, '.*')
 
-  unless dir == 'index'
+  unless dir == 'index' || Dir.exists?(dir)
     Dir.mkdir(dir)
   end
 
   c = File.read(file)
+  c = replace_slim_call(c)
+
   file_contents = Slim::Template.new { c }.render
 
   html = layout.render { file_contents }
