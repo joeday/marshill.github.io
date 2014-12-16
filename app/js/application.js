5e13290c0511af16b4823c0c36d048850a5c5cb7
Joe Day <joe@marshill.com>
2014-12-16 13:42:56 -0800

    scrolling transitions

--- app/js/application.js
+++ app/js/application.js
@@ -4,3 +4,14 @@ $(function() {
 		threshold : 200
 	});
 });
+
+$("#mh_logo").click(function() {
+  $("html, body").animate({ scrollTop: "0" }, 300, "swing");
+});
+
+$('a').click(function(){
+  $('html, body').animate({
+    scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 300, "swing");
+  return false;
+});
