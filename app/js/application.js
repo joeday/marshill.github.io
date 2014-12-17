0eefaddba9dabb4a625ee81e6fec4e5d329e59ac
Joe Day <joe@marshill.com>
2014-12-17 09:06:29 -0800

    nav timeline

--- app/js/application.js
+++ app/js/application.js
@@ -9,9 +9,11 @@ $("#mh_logo").click(function() {
   $("html, body").animate({ scrollTop: "0" }, 300, "swing");
 });
 
-$('a').click(function(){
-  $('html, body').animate({
-    scrollTop: $( $.attr(this, 'href') ).offset().top
-  }, 300, "swing");
-  return false;
-});
+// FIX THIS AFTER NAV IS FULLY BUILT
+
+// $('a').click(function(){
+//   $('html, body').animate({
+//     scrollTop: $( $.attr(this, 'href') ).offset().top
+//   }, 300, "swing");
+//   return false;
+// });
