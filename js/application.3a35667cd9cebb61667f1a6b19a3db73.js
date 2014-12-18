93e95240519399c22a128ce9eed0d9016c5054b8
Lars Walen <lars.walen@marshill.com>
2014-12-18 09:03:03 -0800

    Update

--- js/application.3a35667cd9cebb61667f1a6b19a3db73.js
+++ js/application.3a35667cd9cebb61667f1a6b19a3db73.js
@@ -3,14 +3,30 @@ $(function() {
 		effect : 'fadeIn',
 		threshold : 200
 	});
+  
+  $(".tLink").click(function(e) {
+    e.preventDefault();
+    
+    if ($("#tNav").hasClass("hidden-xs")) {
+      $("#tNav").removeClass("hidden-xs");
+      $("#tNav").addClass("visible-xs");
+    } else {
+      $("#tNav").removeClass("visible-xs");
+      $("#tNav").addClass("hidden-xs");
+    }
+
+  });
+  
 });
+
+
 
 $("#mh_logo").click(function() {
   $("html, body").animate({ scrollTop: "0" }, 300, "swing");
 });
 
 // FIX THIS AFTER NAV IS FULLY BUILT
-
+//
 // $('a').click(function(){
 //   $('html, body').animate({
 //     scrollTop: $( $.attr(this, 'href') ).offset().top
