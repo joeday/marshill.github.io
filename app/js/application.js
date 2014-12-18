70e3e84987e699f192fb4b8370e6dca43d68a4ee
Joe Day <joe@marshill.com>
2014-12-17 17:08:06 -0800

    toggle mobile nav

--- app/js/application.js
+++ app/js/application.js
@@ -3,7 +3,23 @@ $(function() {
 		effect : 'fadeIn',
 		threshold : 200
 	});
+  
+  $(".tLink").click(function(e) {
+    e.preventDefault();
+    
+    if ($("#tNav").hasClass("hidden-xs")) {
+      $("#tNav").removeClass("hidden-xs");
+      $("#tNav").addClass("visible-xs");
+    } else {
+      $("#tNav").removeClass("visible-xs");
+      $("#tNav").addClass("hidden-xs");
+    }
+
+  });
+  
 });
+
+
 
 $("#mh_logo").click(function() {
   $("html, body").animate({ scrollTop: "0" }, 300, "swing");
